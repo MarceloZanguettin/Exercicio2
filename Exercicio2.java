68f6e93656e0c052994ad64d88338eac857bfc0a
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2022-03-14 01:41:08 +0000

    Entregue

---
+++ Exercicio2.java
@@ -0,0 +1,10 @@
+public class Exercicio2{
+
+    public static void main(String[] args){
+        int i=1;
+        for (int n=1; n<=1000; n++){
+            i=i+n;
+            System.out.println(i);
+        }
+    }
+}
